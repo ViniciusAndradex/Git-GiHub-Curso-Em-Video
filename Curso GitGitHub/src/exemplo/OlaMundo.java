5673ff564b42cc28b0aa80fb84b8264e3779469f
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-06-24 17:05:45 -0300

    Adiconado 1 linha
    Foi adicionado mais uma linha de codigo para compreender o conteudo.

--- Curso GitGitHub/src/exemplo/OlaMundo.java
+++ Curso GitGitHub/src/exemplo/OlaMundo.java
@@ -4,5 +4,6 @@ public class OlaMundo {
 	public static void main(String[] args) {
 		System.out.println("Ola, Mundo!");
 		System.out.println("Agora, vai!!!!");
+		System.out.println("Vou comprovar, que Funciona em Linux!!");
 	}
 }
